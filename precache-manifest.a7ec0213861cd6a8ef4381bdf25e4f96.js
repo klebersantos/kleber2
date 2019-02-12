0617832c3a1de4b27c74381a71e1890db72c7475
kleber santos <j.kleber.s@gmail.com>
2019-02-11 21:59:52 -0300

    Updates

---
+++ precache-manifest.a7ec0213861cd6a8ef4381bdf25e4f96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "460e4326401d4ff90f61",
+    "url": "/kleber2/static/css/main.278069db.chunk.css"
+  },
+  {
+    "revision": "460e4326401d4ff90f61",
+    "url": "/kleber2/static/js/main.460e4326.chunk.js"
+  },
+  {
+    "revision": "754aa13b2442a57bcddb",
+    "url": "/kleber2/static/js/runtime~main.754aa13b.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/kleber2/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kleber2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fde155077eae8f13e9799236ef9c2f35",
+    "url": "/kleber2/index.html"
+  }
+];
